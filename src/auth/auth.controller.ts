60fb39400d214e08843f65a33f4679f67cdd0ed7
gautchap <gautchap+github@outlook.com>
2024-01-29 12:35:40 +0100

    feat : add reset password and confirmation signup feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/CreateUser.dto';
 import { LogUserDto } from './dto/LogUser.dto';
 import { AuthService } from './auth.service';
+import { ResetPasswordDto } from './dto/ResetPassword.dto';
+import { ResetPasswordConfirmationDto } from './dto/ResetPasswordConfirmation.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,8 +14,27 @@ export class AuthController {
     return this.authService.CreateUser(CreateUser);
   }
 
+  @Get('signup-confirmation')
+  CreateUserConfirm(@Query('token') token: string) {
+    return this.authService.CreateUserConfirm(token);
+  }
+
   @Post('login')
   LogUser(@Body() LogUser: LogUserDto) {
     return this.authService.LogUser(LogUser);
+  }
+
+  @Post('reset-password')
+  ResetPassword(@Body() ResetPassword: ResetPasswordDto) {
+    return this.authService.ResetPassword(ResetPassword);
+  }
+
+  @Post('reset-password-confirmation')
+  ResetPasswordConfirmation(
+    @Body() ResetPasswordConfirmation: ResetPasswordConfirmationDto,
+  ) {
+    return this.authService.ResetPasswordConfirmation(
+      ResetPasswordConfirmation,
+    );
   }
 }
