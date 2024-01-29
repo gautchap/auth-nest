82a07c2af9ad825d7b4cf3d6a553d5fae9b42192
gautchap <gautchap+github@outlook.com>
2024-01-29 12:37:25 +0100

    refactor: move jwt expire time inside service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,6 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
     }),
   ],
   controllers: [AuthController],
