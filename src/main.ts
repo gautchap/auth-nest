a5ebacf286df48a571734bb51017af51983f97a0
gautchap <gautchap+github@outlook.com>
2024-02-01 17:19:02 +0100

    fix : resolve railway deployment issue

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
 
