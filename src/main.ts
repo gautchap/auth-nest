34182305c4de8b1b5b05e3bbafe71060cd8d62fb
gautchap <gautchap+github@outlook.com>
2024-02-01 15:37:43 +0100

    feat : install swagger

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import compression from '@fastify/compress';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,6 +17,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   await app.register(compression);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('auth endpoints')
+    .setDescription('auth examples')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
